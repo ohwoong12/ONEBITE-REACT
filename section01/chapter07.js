7be8b82c259e34597a796d4b64246e2685733048
OH WOONG KWON <oungtom1212@naver.com>
2025-09-14 22:21:28 +0900

    대입, 산술, 복합 대입, 증감, 논리, 비교 연산자 학습

---
+++ section01/chapter07.js
@@ -0,0 +1,39 @@
+// 1. 대입 연산자
+let var1 = 1;
+
+// 2. 산술 연산자
+let num1 = 3 + 2;
+let num2 = 3 - 2;
+let num3 = 3 * 2;
+let num4 = 3 / 2;
+let num5 = 3 % 2;
+
+// 3. 복합 대입 연산자
+// 산술 연산자와 대입 연산자가 복합되어 있음
+let num6 = 10;
+num6 += 20;
+num6 - +20;
+num6 *= 20;
+num6 /= 20;
+num6 %= 20;
+
+// 4. 증감 연산자
+num7 = 10;
+num7++;
+num7--;
+++num7;
+
+// 5. 논리 연산자
+// Boolean 타입의 값을 다룰 때 사용함
+
+let or = true || false; // 둘 중 하나만 참이면 참
+let and = true && false; // 둘 다 참이어야 참
+let not = !true; // true라면 false, false라면 true
+
+// 6. 비교 연산자
+let comp1 = 1 === 2; // 두 개의 값이 같은지 비교 false
+let comp2 = 1 !== 2; // 두 개의 값이 다른지 비교 true
+let comp3 = 2 > 1; // true
+let comp4 = 2 < 1; // false
+let comp5 = 2 >= 2; // 초과가 아닌 이상 포함
+let comp6 = 2 <= 2; // 미만이 아닌 이하 포함
