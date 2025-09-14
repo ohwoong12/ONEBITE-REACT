cf3fa347363c812e0de8623c236756cbc169aeca
OH WOONG KWON <oungtom1212@naver.com>
2025-09-14 22:28:42 +0900

    Truthy와 Falsy 학습
    1. Falsy한 값
    2. Truthy한 값
    3. 활용 사례 학습

---
+++ section02/chapter01.js
@@ -0,0 +1,39 @@
+// 1. Falsy한 값
+// 총 7개의 값이 존재
+
+let f1 = undefined;
+let f2 = null;
+let f3 = 0;
+let f4 = -0;
+let f5 = NaN;
+let f6 = "";
+let f7 = 0n; // Big Integer라는 특수한 자료형
+
+// if (!f1) {
+//   console.log("falsy");
+// }
+
+// 2. Truthy한 값
+// 7가지 falsy한 값들을 제외한 나머지 모든 값
+let t1 = "hello";
+let t2 = 123;
+let t3 = [];
+let t4 = {};
+let t5 = () => {};
+
+// if (t5) {
+//   console.log("truthy");
+// }
+
+// 3. 활용 사례
+function printName(person) {
+  if (!person) {
+    console.log("person의 값이 없음");
+    return; // 함수 종료
+  }
+  console.log(person.name);
+}
+
+//let person = { name: "권오웅" };
+let person = null;
+printName(person);
