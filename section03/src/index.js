b0741c0bcc29041d578ec058b7fab96d55ec4e2e
OH WOONG KWON <oungtom1212@naver.com>
2025-09-17 10:40:38 +0900

    학습을 위한 index.html

---
+++ section03/src/index.js
@@ -0,0 +1,16 @@
+// index.js
+
+// const { add, sub } = require("./math");
+// import multiply from "./math.js";
+// import mul from "./math.js";
+// import { add, sub } from "./math.js";
+import mul, { add, sub } from "./math.js";
+
+// console.log(add(1, 2));
+// console.log(sub(2, 1));
+// console.log(mul(2, 2));
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
