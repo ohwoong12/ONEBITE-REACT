e69f64d03c10ab76292d8e89069ebcbacdadbee2
OH WOONG KWON <oungtom1212@naver.com>
2025-09-19 20:39:18 +0900

    이벤트 핸들러를 통한 이벤트 처리 학습

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,6 +1,18 @@
 const Button = ({ children, text, color = "black" }) => {
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      // onClick={() => {
+      //   console.log(text);
+      // }}
+      onClick={onClickButton}
+      //onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text}-{color.toUpperCase()}
       {children}
     </button>
