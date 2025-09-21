dc4d6e62c69bbee3c5fe13eee590dc4476c9af1c
OH WOONG KWON <oungtom1212@naver.com>
2025-09-21 21:12:52 +0900

    useEffect로 라이프사이클 제어하기 학습
    - React의 라이프사이클 각 단계별로 코드를 작성하여 학습
    - 최초로 실행될 때를 제외한 실제로 업데이트 될 때만 update가 출력되는 코드 구현
    - unmount 단계의 출력을 위한 Even 컴포넌트 생성 후 정리 함수 작성

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,21 +1,31 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import { useState, useEffect } from "react";
+import Even from "./components/Even";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const isMount = useRef(false);
+
   // 라이프사이클의 생애주기
   // 1. 마운트: 탄생
-  // 2. 업데이트 : 변화, 리렌더링
-  // 3. 언마운트: 죽음
-
-  
-
   useEffect(() => {
-    console.log(`count: ${count}`);
-  }, [count]);
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트: 죽음
+  
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -29,6 +39,7 @@ function App() {
       <h1>Simple Counter</h1>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller
