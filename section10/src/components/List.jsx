528859c3420728afa1178bf1247503c4901c25a6
OH WOONG KWON <oungtom1212@naver.com>
2025-09-26 20:24:05 +0900

    useMemo를 통한 불필요한 연산 방지 학습
    - useMemo라는 새로운 React Hook 학습
    - getAnalyzedData() 함수 생성 후 연산 관찰
    - 반복적인 연산 방지 위해서 useMemo로 새롭게 생성 후 학습

--- section10/src/components/List.jsx
+++ section10/src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import ToDoItem from "./ToDoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
@@ -25,9 +25,62 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
+  // const getAnalyzedData = () => {
+  //   // 현재 등록된 ToDo 아이템의 개수를 저장
+  //   const totalCount = todos.length;
+  //   // 전체 ToDo들 중에 완료된 ToDo의 개수를 저장
+  //   // filter 메서드를 사용하여 isDone 프로퍼티가 참인 값만 필터링
+  //   const doneCount = todos.filter(
+  //     (todo) => todo.isDone,
+  //   ).length;
+  //   const notDoneCount = totalCount - doneCount;
+
+  //   return {
+  //     totalCount,
+  //     doneCount,
+  //     notDoneCount,
+  //   };
+  // };
+
+  // 기존에 따로 구조 분해 할당은 없앤 후 바로 받아옴
+  const { totalCount, doneCount, notDoneCount } =
+    useMemo(() => {
+      console.log("연산 실행!");
+
+      // 현재 등록된 ToDo 아이템의 개수를 저장
+      const totalCount = todos.length;
+      // 전체 ToDo들 중에 완료된 ToDo의 개수를 저장
+      // filter 메서드를 사용하여 isDone 프로퍼티가 참인 값만 필터링
+      const doneCount = todos.filter(
+        (todo) => todo.isDone,
+      ).length;
+      const notDoneCount = totalCount - doneCount;
+
+      return {
+        totalCount,
+        doneCount,
+        notDoneCount,
+      };
+    }, [todos]);
+  // 두번째 인수: 의존성 배열 (deps)
+  // deps의 값이 비어있다면 처음에 컴포넌트가 렌더링 될때 딱 한번만 실행
+  // deps의 값이 변했을 때만 콜백 함수를 다시 실행함
+  // deps 배열을 아예 생략하면 리렌더링 될 때마다 실행함
+  //  => 쓰는 의미가 없음
+
+  // 콜백 함수가 반환하는 값을 useMemo가 다시 그대로 반환해줌
+
+  // const { totalCount, doneCount, notDoneCount } =
+  //   getAnalyzedData();
+
   return (
     <div className="List">
       <h4>Todo List🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
