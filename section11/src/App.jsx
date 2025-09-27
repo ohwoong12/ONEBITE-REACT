c180784b1fe510378c31dbac0514d3abba79d3d6
OH WOONG KWON <oungtom1212@naver.com>
2025-09-27 16:06:45 +0900

    섹션 11 학습을 위한 프로젝트 사전준비
    - 기존에 계속 수정하던 ToDoList 앱 사용

---
+++ section11/src/App.jsx
@@ -0,0 +1,95 @@
+import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+import { useRef, useReducer, useCallback } from "react";
+
+// 모조 데이터 선언
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
+// 실제로 처리하게 될 함수
+// 액션 객체의 값에 따라 변화된 state의 값을 return
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      // action.data: dispatch 함수를 호출하면서 전달한 새로운 ToDo 아이템
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId
+          ? { ...item, isDone: !item.isDone }
+          : item,
+      );
+    case "DELETE":
+      return state.filter(
+        (item) => item.id != action.targetId,
+      );
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  const onCreate = useCallback((content) => {
+    dispatch({
+      type: "CREATE",
+      // 새롭게 생성할 ToDo 아이템을 객체로 삽입
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  }, []);
+
+  const onUpdate = useCallback((targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
+  }, []);
+
+  const onDelete = useCallback((targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List
+        todos={todos}
+        onUpdate={onUpdate}
+        onDelete={onDelete}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ section11/src/components/List.jsx
@@ -0,0 +1,105 @@
+import "./List.css";
+import ToDoItem from "./ToDoItem";
+import { useState, useMemo } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  // 매개변수로 이벤트 객체를 받아서 setSearch를 호출
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    } else {
+      return todos.filter((todo) =>
+        // 소문자로 변환 후 검색한 내용만 필터링
+        todo.content
+          .toLowerCase()
+          .includes(search.toLowerCase()),
+      );
+    }
+  };
+
+  const filteredTodos = getFilteredData();
+
+  // const getAnalyzedData = () => {
+  //   // 현재 등록된 ToDo 아이템의 개수를 저장
+  //   const totalCount = todos.length;
+  //   // 전체 ToDo들 중에 완료된 ToDo의 개수를 저장
+  //   // filter 메서드를 사용하여 isDone 프로퍼티가 참인 값만 필터링
+  //   const doneCount = todos.filter(
+  //     (todo) => todo.isDone,
+  //   ).length;
+  //   const notDoneCount = totalCount - doneCount;
+
+  //   return {
+  //     totalCount,
+  //     doneCount,
+  //     notDoneCount,
+  //   };
+  // };
+
+  // 기존에 따로 구조 분해 할당은 없앤 후 바로 받아옴
+  const { totalCount, doneCount, notDoneCount } =
+    useMemo(() => {
+      console.log("연산 실행!");
+
+      // 현재 등록된 ToDo 아이템의 개수를 저장
+      const totalCount = todos.length;
+      // 전체 ToDo들 중에 완료된 ToDo의 개수를 저장
+      // filter 메서드를 사용하여 isDone 프로퍼티가 참인 값만 필터링
+      const doneCount = todos.filter(
+        (todo) => todo.isDone,
+      ).length;
+      const notDoneCount = totalCount - doneCount;
+
+      return {
+        totalCount,
+        doneCount,
+        notDoneCount,
+      };
+    }, [todos]);
+  // 두번째 인수: 의존성 배열 (deps)
+  // deps의 값이 비어있다면 처음에 컴포넌트가 렌더링 될때 딱 한번만 실행
+  // deps의 값이 변했을 때만 콜백 함수를 다시 실행함
+  // deps 배열을 아예 생략하면 리렌더링 될 때마다 실행함
+  //  => 쓰는 의미가 없음
+
+  // 콜백 함수가 반환하는 값을 useMemo가 다시 그대로 반환해줌
+
+  // const { totalCount, doneCount, notDoneCount } =
+  //   getAnalyzedData();
+
+  return (
+    <div className="List">
+      <h4>Todo List🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <ToDoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
